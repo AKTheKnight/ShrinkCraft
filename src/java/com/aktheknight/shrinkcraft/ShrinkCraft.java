c0b8f702d14a5f151a15d7fbe88ca1c7bddf7251
AKTheKnight <AKTheKnight.minecraft@gmail.com>
2016-01-04 21:04:15 +0000

    Added World Gen

--- src/java/com/aktheknight/shrinkcraft/ShrinkCraft.java
+++ src/java/com/aktheknight/shrinkcraft/ShrinkCraft.java
@@ -4,7 +4,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 
@@ -24,5 +26,10 @@ public class ShrinkCraft {
 	public void preInit(FMLPreInitializationEvent event) {
 		ModItems.init();
 		ModBlocks.init();
+	}
+	
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent e) {
+	    GameRegistry.registerWorldGenerator(new ModWorldGen(), 0);
 	}
 }
